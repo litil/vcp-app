11b4a8fd08f3675cc12c3dafa9ad2d66c97333cc
Guillaume Lambert <guillaume.p.lambert@gmail.com>
2016-06-05 16:40:13 +0200

    Added a test on rejection.data to fix error displayed in the console when the data returned by the jsonp.xsl call is not valid

--- public/app/app.js
+++ public/app/app.js
@@ -24,7 +24,7 @@
 						// Loop through each rejection reason and redirect to the login
 						// state if one is encountered
 						angular.forEach(rejectionReasons, function(value, key) {
-							if(rejection.data.error === value) {
+							if(rejection.data != undefined && rejection.data.error === value) {
 								// If we get a rejection corresponding to one of the reasons
 								// in our array, we know we need to authenticate the user so
 								// we can remove the current user from local storage
